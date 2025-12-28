c2b84297d5b426d39d2130af952b194487812986
Dominik Farr <dominik.farr@icloud.com>
2025-12-28 15:26:31 -0800

    .

--- main.py
+++ main.py
@@ -4,15 +4,20 @@ from nn_utils import forward
 from inspect_weights import load_weights_as_dict
 
 
+def sample(logits: jax.Array) -> jax.Array:
+    return jnp.argmax(logits)
+
+
 def main():
     vocab_size = 262144
-    token_ids = jnp.array([2, 9259])  # <BOS> Hello
+    token_ids = jnp.array([2, 81115])  # <BOS> Hell
     xs = jax.nn.one_hot(token_ids, vocab_size)
 
     params = load_weights_as_dict("model_stacked.safetensors")
     xs = forward(xs, params)
 
-    print(xs)
+    predicted = jax.vmap(sample)(xs)
+    print(predicted)
 
 
 if __name__ == "__main__":
