8e1e8ad012d8ee3db15d6674484030d7d55fa346
Dominik Farr <dominik.farr@icloud.com>
2026-01-04 13:45:49 -0800

    .

--- supervised_train.py
+++ supervised_train.py
@@ -17,8 +17,40 @@ But for now we just always take sequences of the same length to avoid this probl
 """
 
 import jax
+import jax.numpy as jnp
+from gemma_forward import forward
+from inspect_weights import load_weights_as_dict
+import optax
+from gemma_forward import Params
+from sft_data import get_training_batch
 
 
-def loss(xs, params) -> jax.Array:
-    pass
+def loss_fn(xs, params) -> jax.Array:
+    predictions = forward(xs, params)
+    input_len = xs.shape[0]
+    predictions = predictions[: input_len - 1]  # remove padding
+    labels = xs[1:]
+    loss = optax.softmax_cross_entropy_with_integer_labels(predictions, labels)
+    return jnp.mean(loss)
 
+
+def SGD(params, grads, lr) -> Params:
+    return jax.tree_util.tree_map(lambda param, grad: param - lr * grad, params, grads)
+
+
+def train(key, batch_size, params, seq_length) -> Params:
+    def loss_batched(xss, params) -> jax.Array:
+        return jnp.mean(jax.vmap(loss_fn, in_axes=(0, None))(xss, params))
+
+    train_data = get_training_batch(key, batch_size, seq_length)
+    loss, grads = jax.value_and_grad(loss_batched, argnums=1)(train_data, params)
+    print(loss)
+    return SGD(params, grads, 0.01)
+
+
+# TESTING
+if __name__ == "__main__":
+    key = jax.random.key(42)
+    params = load_weights_as_dict("model_stacked_pt.safetensors")
+
+    print(train(key, 2, params, 4))
