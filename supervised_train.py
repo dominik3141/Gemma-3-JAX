7fc06ec3f50cd2a13f43bb9d8535a166d5ca04d0
Dominik Farr <dominik.farr@icloud.com>
2026-01-05 16:41:19 -0800

    .

--- supervised_train.py
+++ supervised_train.py
@@ -50,15 +50,14 @@ def train(
         return jnp.mean(jax.vmap(loss_fn, in_axes=(0, None))(xss, params))
 
     train_data = get_training_batch(key, batch_size, seq_length)
-
     train_data = jax.lax.with_sharding_constraint(train_data, data_sharding)
 
     loss, grads = jax.value_and_grad(loss_batched, argnums=1)(train_data, params)
     return SGD(params, grads, lr), loss
 
 
-def train_loop(data_sharding, params, key) -> tuple[Params, jax.Array]:
-    new_params, loss = train(key, 4 * 2, params, 1024, 0.01, data_sharding)
+def train_loop(data_sharding, batch_size, params, key) -> tuple[Params, jax.Array]:
+    new_params, loss = train(key, batch_size, params, 1024, 0.01, data_sharding)
 
     return new_params, loss
 
@@ -74,26 +73,27 @@ def main():
     params = load_weights_as_dict("model_stacked_pt.safetensors")
 
     # Distributed training
-    num_devices = jax.device_count()
+    num_devices: int = jax.device_count()
     print(f"Number of devices {num_devices}")
     device_mesh = mesh_utils.create_device_mesh((num_devices,))
     mesh = Mesh(device_mesh, axis_names=("batch",))
     data_sharding = NamedSharding(mesh, P("batch"))
     param_sharding = NamedSharding(mesh, P())
 
-    # Create a dummy batch to check sharding
-    test_batch = get_training_batch(key, 16, 64)
-    test_batch = jax.device_put(test_batch, data_sharding)
-    print("Batch Sharding Visualization:")
-    jax.debug.visualize_array_sharding(test_batch)
-
     # ensure parameters are replicated across all cores
     params = jax.device_put(params, param_sharding)
 
+    # Config
+    batch_size = num_devices * 2
+
     # do stuff
-    keys = jax.random.split(key, 4 * 100)
+    keys = jax.random.split(key, num_devices * 100)
     with mesh:
-        params, losses = jax.lax.scan(partial(train_loop, data_sharding), params, keys)
+        params, losses = jax.lax.scan(
+            partial(train_loop, data_sharding, batch_size),
+            params,
+            keys,
+        )
     print("XLA retuned control")
     print(losses)
 
