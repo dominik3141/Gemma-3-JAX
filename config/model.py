1e2fcf11689e4ab3e4313bc39d0ee956c71dbd84
Dominik Farr <dominik.farr@icloud.com>
2026-02-10 13:12:25 -0800

    add config for 1b model

--- config/model.py
+++ config/model.py
@@ -11,3 +11,15 @@ gemma_3_27b = SimpleNamespace(
     d_mlp=21504,
     sliding_window=1024,
 )
+
+gemma_3_1b = SimpleNamespace(
+    num_attention_heads=4,
+    num_key_value_heads=1,
+    num_queries_per_group=4,
+    num_layers=26,
+    d_model=1152,
+    d_kvq=256,
+    head_dim=256,
+    d_mlp=6912,
+    sliding_window=1024,
+)
